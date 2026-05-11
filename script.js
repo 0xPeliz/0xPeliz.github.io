41d4aedb4f985de681157ddee25124bbb5d008d7
mattew <mattiapel04@gmail.com>
2026-05-11 21:43:05 +0200

    init website

---
+++ script.js
@@ -0,0 +1,12 @@
+// Esempio di interazione: Smooth Scroll per i link della nav
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("Portfolio caricato con successo!");
